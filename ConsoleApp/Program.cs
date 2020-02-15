03b2853692afdc8a847415b4e3b42300edfb3a32
Evgeny Moskovchenko <EvgenyMosk@outlook.com>
2020-02-15 16:25:50 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YVMS_SC.ConsoleApp {
+	public class Program {
+		private static void Main(String[] args) {
+		}
+	}
+}
